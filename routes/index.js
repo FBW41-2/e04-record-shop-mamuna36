a50e5cf4bbc639d453631b97be730ed3d28cd4e5
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-04-20 15:34:32 +0200

    lowdb added

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const lowdb = require("lowdb");
-const records = [
-  {
-    name: "Ali Zafer",
-    title: "Tu hi hai",
-    year: "2003",
-  },
-  {
-    name: "Atif",
-    title: "Tera honay laga hun",
-    year: "2008",
-  },
-];
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("./data/db.json");
+const db = low(adapter);
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("index", { title: "Express" });
 });
-router.get("/api/records", function (req, res, next) {
-  res.json(records);
+router.get("/api/records", (req, res, next) => {
+  res.json(db);
 });
-router.post("/api/records", function (req, res, next) {
-  records.push({
+router.post("/api/records", (req, res, next) => {
+  db.push({
     name: req.body.name,
     title: req.body.title,
     year: req.body.year,
-  });
+  }).write();
   res.redirect("/api/records");
 });
 module.exports = router;
