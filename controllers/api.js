a3952195066e1d9e2673e2d3ed1e9e34fc7ea902
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-04-21 12:11:30 +0200

    database reformated

--- controllers/api.js
+++ controllers/api.js
@@ -1,11 +1,18 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("./data/db.json");
+const db = low(adapter);
 exports.recordsList = function (req, res) {
-  res.json(db);
+  res.json(db.get("records").value());
 };
 exports.createRecords = function (req, res) {
-  db.push({
-    name: req.body.name,
-    title: req.body.title,
-    year: req.body.year,
-  }).write();
+  db.get("records")
+    .push({
+      name: req.body.name,
+      title: req.body.title,
+      year: req.body.year,
+    })
+    .write();
   res.redirect("/api/records");
 };
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
 const { recordsList, createRecords } = require("../controllers/api");
 
-const adapter = new FileSync("./data/db.json");
-const db = low(adapter);
 router.route("/records").get(recordsList).post(createRecords);
 
 module.exports = router;
