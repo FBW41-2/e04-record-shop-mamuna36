ad8d29e9a8f97ab603cbad52652ff54e4dafa044
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-04-22 04:42:47 +0200

    id routes added

--- routes/records.js
+++ routes/records.js
@@ -1,24 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("data/db.json");
-const db = low(adapter);
 const {
   getRecords,
+  getRecord,
+  updateRecord,
+  deleteRecord,
   addRecord,
-  findRecord,
 } = require("../controllers/recordsController");
 
-/**
- * GET all records
- */
-router.get("/", getRecords);
+router.route("/").get(getRecords).post(addRecord);
 
-/**
- * POST a record
- */
-router.post("/", addRecord);
-// router.get("/:id", findRecord);
+router.route("/:id").get(getRecord).delete(deleteRecord).put(updateRecord);
 
 module.exports = router;
