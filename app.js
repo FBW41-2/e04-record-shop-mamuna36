292d0ae1bb809e79ec34060d88f5bacfad65f0b1
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-04-24 04:14:01 +0200

    error handling added

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 /** EXTERNAL DEPENDENCIES */
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
 /** ROUTERS */
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const recordsRouter = require('./routes/records');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const recordsRouter = require("./routes/records");
 const { setCors } = require("./middleware/security");
 
 /** INIT */
 const app = express();
 
 /** LOGGING */
-app.use(logger('dev'));
-
+app.use(logger("dev"));
 
 /** SETTING UP LOWDB */
-const adapter = new FileSync('data/db.json');
+const adapter = new FileSync("data/db.json");
 const db = low(adapter);
-db.defaults({ records:[] }).write();
-
+db.defaults({ records: [] }).write();
 
 /** REQUEST PARSERS */
 app.use(express.json());
@@ -32,13 +30,26 @@ app.use(cookieParser());
 app.use(setCors);
 
 /** STATIC FILES*/
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 /** ROUTES */
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/records', recordsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/records", recordsRouter);
+/** ERROR HANDLING */
+app.use(function (req, res, next) {
+  const error = new Error("Looks like something broke...");
+  error.status = 400;
+  next(error);
+});
+
+app.use(function (err, req, res, next) {
+  res.send({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 /** EXPORT PATH */
 module.exports = app;
