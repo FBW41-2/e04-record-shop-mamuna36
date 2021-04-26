da070ca0c34703af8ce7e38a7cb3b6295e0aac85
Mamuna Anwar <mamuna_anwar@hotmail.com>
2021-04-26 13:56:32 +0200

    mongodb crud operations added

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,39 +1,60 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('data/db.json');
-const db = low(adapter);
+const mongodb = require("mongodb");
 
-
+// get all records
 exports.getRecords = (req, res, next) => {
-    const records = db.get('records').value()
-    res.status(200).send(records);
-}
+  // access db from global object   // select all records
+  req.app.locals.db
+    .collection("records")
+    .find()
+    .toArray((err, docs) => {
+      res.json(docs);
+    });
+};
 
+// get specific record
 exports.getRecord = (req, res, next) => {
-    const { id } = req.params;
-    const record = db.get('records').find({ id });
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  req.app.locals.db
+    .collection("records")
+    .findOne({ _id: new mongodb.ObjectID(id) }, (err, result) => {
+      res.json(result);
+    });
+};
 
+// delete one record
 exports.deleteRecord = (req, res, next) => {
-    const { id } = req.params;
-    const record = db.get('records').remove({ id }).write();
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  req.app.locals.db
+    .collection("records")
+    .deleteOne({ _id: new mongodb.ObjectID(id) }, (err, result) => {
+      if (err) console.error(err);
+      console.log("del result", result);
+      res.json({ deleted: result.deletedCount });
+    });
+};
 
+// update one record
 exports.updateRecord = (req, res, next) => {
-    const { id } = req.params;
-    const dt = req.body;
-    const record = db.get('records').find({ id }).assign(dt).write();
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  req.app.locals.db.collection("records").updateOne(
+    // filter
+    { _id: new mongodb.ObjectID(id) },
+    // new data
+    {
+      $set: req.body,
+    },
+    // callback function
+    (err, entry) => {
+      res.json(entry);
+    }
+  );
+};
 
+// create new record
 exports.addRecord = (req, res, next) => {
-    const record = req.body;
-    db.get('records').push(record)
-        .last()
-        .assign({ id: Date.now().toString() })
-        .write()
-
-    res.status(200).send(record);
-}
+  const record = req.body;
+  // access db from global object
+  req.app.locals.db.collection("records").insertOne(record, (err, entry) => {
+    res.json(entry);
+  });
+};
